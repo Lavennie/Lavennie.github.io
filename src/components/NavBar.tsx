bc86f4b1d1b554d63b94d4c4b335697cda8d3a85
Lavennie <42303947+Lavennie@users.noreply.github.com>
2026-02-10 23:45:17 +0100

    Dummy navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+export default function NavBar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-inner">
+                <ul className="nav-links">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#home">Art</a></li>
+                    <li><a href="#home">Coding</a></li>
+                    <li><a href="#home">Other</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
