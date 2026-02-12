c5a4504b47fb81a4b46536bf36ac8084659dfea8
Lavennie <42303947+Lavennie@users.noreply.github.com>
2026-02-12 15:07:08 +0100

    Banner, navbar, axolotl, background

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import {useState} from 'react'
 import {Canvas} from '@react-three/fiber'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 import Axolotl3D from './components/Axolotl3D.tsx'
 import NavBar from './components/NavBar.tsx'
@@ -13,46 +11,30 @@ function App() {
 
     return (
         <>
-            { /* 3D OBJECTS - axolotl */ }
-            <Canvas
-                style={{
-                    position: 'fixed',
-                    inset: 0,
-                    transform: "scale(1.3)",
-                    transformOrigin: "110% 120%",
-                    pointerEvents: 'none', // allows clicking through
-                    zIndex: 0,
-                }}
-                camera={{position: [0, 1, 8]}}
-            >
-                <ambientLight intensity={0.5}/>
-                <Axolotl3D/>
-            </Canvas>
-
-            { /* SITE */ }
             <NavBar />
+            { /* SITE */ }
             <div className="app-background">
+
                 <div className="site-container">
-                    <div>
-                        <a href="https://vite.dev" target="_blank">
-                            <img src={viteLogo} className="logo" alt="Vite logo"/>
-                        </a>
-                        <a href="https://react.dev" target="_blank">
-                            <img src={reactLogo} className="logo react" alt="React logo"/>
-                        </a>
+
+                    { /* axolotl swimming - fixed size image */ }
+                    <div className="axolotl-scene">
+                        { /* 3D OBJECTS - axolotl */ }
+                        <Canvas
+                            style={{
+                                position: 'absolute',
+                                pointerEvents: 'none', // allows clicking through
+                                top: 250,
+                                height: "70%",
+                                width: "90%",
+                                margin: "auto",
+                            }}
+                            camera={{position: [0, 1, 8], zoom: 2.5}}
+                        >
+                            <ambientLight intensity={0.5}/>
+                            <Axolotl3D/>
+                        </Canvas>
                     </div>
-                    <h1>Vite + React</h1>
-                    <div className="card">
-                        <button onClick={() => setCount((count) => count + 1)}>
-                            count is {count}
-                        </button>
-                        <p>
-                            Edit <code>src/App.tsx</code> and save to test HMR
-                        </p>
-                    </div>
-                    <p className="read-the-docs">
-                        Click on the Vite and React logos to learn more
-                    </p>
                 </div>
             </div>
         </>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,24 @@
+import './NavBar.css'
+import nameBase from '.././assets/Banner Name Base.png'
+import nameOutline from '.././assets/Banner Name Outline.png'
+
 export default function NavBar() {
     return (
-        <nav className="navbar">
-            <div className="navbar-inner">
-                <ul className="nav-links">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#home">Art</a></li>
-                    <li><a href="#home">Coding</a></li>
-                    <li><a href="#home">Other</a></li>
-                </ul>
+        <>
+            <div className="banner">
+                <img src={nameBase}/>
+                <img src={nameOutline}/>
             </div>
-        </nav>
+            <nav className="navbar">
+                <div className="navbar-inner">
+                    <ul className="nav-links">
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#home">Art</a></li>
+                        <li><a href="#home">Coding</a></li>
+                        <li><a href="#home">Other</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </>
     );
 }
