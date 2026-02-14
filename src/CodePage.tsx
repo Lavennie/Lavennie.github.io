9d3042c58bc892f3e887b001a32d8409d56285c3
Lavennie <42303947+Lavennie@users.noreply.github.com>
2026-02-14 13:13:03 +0100

    Projects: resize images, add another project

--- src/CodePage.tsx
+++ src/CodePage.tsx
@@ -7,48 +7,56 @@ export default function CodePage() {
     return(<>
         <NavBar colorMain={"#F9FEE5"} colorSide={"#D0FFB4"} textColor={"#7ADF3E"} />
         <div className="app-background">
-            <div className="site-container" style={{height: "1620px"}}>
+            <div className="site-container" style={{height: "1940px"}}>
                 <Project
                     id="Our Cure"
                     imageUrl="project_our_cure.png"
                     description="Video game\n2D tactical RPG"
                     link="https://celestide-games.github.io/index.html"
-                    imgx={-240}
-                    imgy={-230}
-                />
-                <Project
-                    id="Pixel Paint"
-                    imageUrl="project_pixel_paint.png"
-                    description="Unity extension\n2D painting"
-                    imgx={-350}
-                    imgy={-30}
-                    side="left"
-                />
-                <Project
-                    id="Game Engine"
-                    imageUrl="project_game_engine.png"
-                    description="3D animated meshes\nLighting\nCollision detection"
-                    imgx={-600}
-                    imgy={-500}
+                    imgx={0}
+                    imgy={0}
                 />
                 <Project
                     id="GNN Fish"
                     imageUrl="project_mlg_fish.png"
                     description="Graph neural networks\nFish trajectory prediction"
-                    imgx={600}
-                    imgy={300}
+                    imgx={0}
+                    imgy={0}
                     side="left"
                     logoHue={40}
                 />
                 <Project
+                    id="Pixel Paint"
+                    imageUrl="project_pixel_paint.png"
+                    description="Unity extension\n2D painting"
+                    imgx={-300}
+                    imgy={0}
+                />
+                <Project
                     id="MPA"
-                    imageUrl="https://img.itch.zone/aW1hZ2UvMTAzNTI2MC81OTE0OTAzLnBuZw==/original/68MUNc.png"
+                    imageUrl="project_mpa.png"
                     description="3D animated meshes\nLighting\nCollision detection"
                     link="https://lavennie.itch.io/method-process-algorithm"
                     githubLink="https://lavennie.itch.io/method-process-algorithm"
-                    imgx={0}
-                    imgy={-600}
+                    imgx={200}
+                    imgy={0}
+                    side="left"
                     logoHue={40}
+                />
+                <Project
+                    id="Game Engine"
+                    imageUrl="project_game_engine.png"
+                    description="3D animated meshes\nLighting\nCollision detection"
+                    imgx={100}
+                    imgy={0}
+                />
+                <Project
+                    id="Physics Character"
+                    imageUrl="project_physics_character.png"
+                    description="Physics Controlled"
+                    imgx={100}
+                    imgy={0}
+                    side="left"
                 />
             </div>
         </div>
