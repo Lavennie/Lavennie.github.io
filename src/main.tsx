c4d55ba8020767eb4f1eaeb2aaa4467062ea76cd
Lavennie <42303947+Lavennie@users.noreply.github.com>
2026-02-16 16:47:46 +0100

    Hash routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Routes, Route } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import './index.css'
 import HomePage from "./HomePage.tsx";
 import ArtPage from './ArtPage.tsx'
@@ -10,13 +10,13 @@ import OtherPage from "./OtherPage.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <BrowserRouter>
+      <HashRouter>
           <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/art" element={<ArtPage />} />
               <Route path="/projects" element={<CodePage />} />
               <Route path="/other" element={<OtherPage />} />
           </Routes>
-      </BrowserRouter>
+      </HashRouter>
   </StrictMode>,
 )
