25f2489dadb91169a31afbaf3604cda66d6f221e
Lavennie <42303947+Lavennie@users.noreply.github.com>
2026-02-16 17:18:25 +0100

    Fix: slideshow links

--- src/components/home/Slideshow.tsx
+++ src/components/home/Slideshow.tsx
@@ -13,13 +13,13 @@ const slides: Slideshow[] = [
         title: "Art",
         description: "Illustrations  ·  Time-lapse",
         image: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/902fc45c-deef-4f0b-b2ac-8e99318e9ac7/dlbvhqg-c0e27d2b-b431-45f8-862d-b4b2adef77b6.jpg/v1/fill/w_1192,h_670,q_70,strp/kishiar_la_orr_at_a_party_by_lavennielil_dlbvhqg-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6Ii9mLzkwMmZjNDVjLWRlZWYtNGYwYi1iMmFjLThlOTkzMThlOWFjNy9kbGJ2aHFnLWMwZTI3ZDJiLWI0MzEtNDVmOC04NjJkLWI0YjJhZGVmNzdiNi5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.tyBjAvDM0gKt7ChZyKCNYIhz1gw2D9_61ty2WL7F6K0",
-        link: "/art",
+        link: "/#/art",
     },
     {
         title: "Coding",
         description: "Game Development · Procedural Generation · Simulations · Shaders",
         image: "slideshow/project_physics_character.png",
-        link: "/projects",
+        link: "/#/projects",
     }
 ];
 
