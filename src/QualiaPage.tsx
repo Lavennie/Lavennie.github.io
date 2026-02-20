b396322b4f20401f8e1cf40f1e8423522bcd743a
Lavennie <42303947+Lavennie@users.noreply.github.com>
2026-02-20 23:31:58 +0100

    Rename other to qualia

--- src/QualiaPage.tsx
+++ src/QualiaPage.tsx
@@ -1,9 +1,9 @@
-import styles from './OtherPage.module.css'
+import styles from './QualiaPage.module.css'
 import NavBar from "./components/NavBar.tsx";
 import Footer from "./components/Footer.tsx";
 import Constellations from "./components/constellation/Constellations.tsx";
 
-export default function OtherPage() {
+export default function QualiaPage() {
     return(<>
         <NavBar colorMain={"#212125"} colorSide={"#323245"} textColor={"#7AB7FF"} bannerY={36}
                 bannerUrl={"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/902fc45c-deef-4f0b-b2ac-8e99318e9ac7/dl69nr1-0528666b-6243-44c2-bf57-6d6dffb3ff3d.jpg/v1/fill/w_1192,h_670,q_70,strp/man_black_sea_by_lavennielil_dl69nr1-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6Ii9mLzkwMmZjNDVjLWRlZWYtNGYwYi1iMmFjLThlOTkzMThlOWFjNy9kbDY5bnIxLTA1Mjg2NjZiLTYyNDMtNDRjMi1iZjU3LTZkNmRmZmIzZmYzZC5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.ybbnILZkM8S8oCnBdY0S6RixC9sXe2D1Eh5GrUGX51s"} />
