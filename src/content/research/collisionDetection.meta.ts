6085550486a5bf180104ec2cb0cb36eb2841a61f
Lavennie <42303947+Lavennie@users.noreply.github.com>
2026-03-12 15:07:40 +0100

    Added GNNs to research

--- src/content/research/collisionDetection.meta.ts
+++ src/content/research/collisionDetection.meta.ts
@@ -6,8 +6,8 @@ const meta: ResearchMeta = {
     title: "Collision Detection",
     dateStart: "?/?/?",
     dateEnd: "?/?/?",
-    description: "Create collision detection of axis-aligned bounding boxes, oriented bounding boxes and bounding spheres using the separating axis theorem.",
-    image: "research_colliison_detection",
+    description: "Created collision detection of axis-aligned bounding boxes, oriented bounding boxes and bounding spheres using the separating axis theorem.",
+    image: "research_collison_detection",
     tags: [{content: "Physics engine", color: "#EB3B28"}],
     state: "concluded",
 };
---
+++ src/content/research/mlgFish.meta.ts
@@ -0,0 +1,16 @@
+import type {ResearchMeta} from "../types.ts";
+
+const meta: ResearchMeta = {
+    type: "research",
+    id: "graph neural networks",
+    title: "Graph NN",
+    dateStart: "2025/11/01",
+    dateEnd: "2025/12/30",
+    description: "Checked out Graph Neural Networks with focus on solving spatiotemporal problems. Looked into GCN, GraphSage, GAT, transformers on graphs and some other temporal models.",
+    image: "research_mlg_fish",
+    link: "https://medium.com/@anja.laharnar/prediction-fish-swarm-with-graph-neural-network-42da52ce5780",
+    tags: [{content: "AI", color: "#EB3B28"}],
+    state: "concluded",
+};
+
+export default meta;
