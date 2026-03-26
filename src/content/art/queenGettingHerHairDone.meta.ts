d3f7f042c8e66e16da75d059362ebba97fd1558a
Lavennie <42303947+Lavennie@users.noreply.github.com>
2026-03-26 19:45:00 +0100

    Upload art piece

---
+++ src/content/art/queenGettingHerHairDone.meta.ts
@@ -0,0 +1,15 @@
+import type { ArtMeta } from "../types";
+
+const meta: ArtMeta = {
+    type: "art",
+    id: "queen getting her hair done",
+    title: "A tyrant queen",
+    dateEnd: "2026/03/26",
+    description: "",
+    link: "https://www.youtube.com/watch?v=uD2XSjhN07w",
+    image: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/902fc45c-deef-4f0b-b2ac-8e99318e9ac7/dlqcymh-7beff34e-3d47-47ca-9228-9268da3ecb15.jpg/v1/fill/w_1192,h_670,q_70,strp/getting_her_hair_done_by_lavennielil_dlqcymh-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6Ii9mLzkwMmZjNDVjLWRlZWYtNGYwYi1iMmFjLThlOTkzMThlOWFjNy9kbHFjeW1oLTdiZWZmMzRlLTNkNDctNDdjYS05MjI4LTkyNjhkYTNlY2IxNS5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.GCZUqRRgfbJxKMasL9m29BbcaOR0paNU7p3Ngua-5DU",
+    x: 70,
+    yGapAfter: 30
+};
+
+export default meta;
