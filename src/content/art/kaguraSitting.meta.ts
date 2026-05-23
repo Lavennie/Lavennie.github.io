719bd8a708136a887dc9f342e61156ac3c102405
Lavennie <42303947+Lavennie@users.noreply.github.com>
2026-05-23 23:51:08 +0200

    Add Kagura art

---
+++ src/content/art/kaguraSitting.meta.ts
@@ -0,0 +1,15 @@
+import type { ArtMeta } from "../types";
+
+const meta: ArtMeta = {
+    type: "art",
+    id: "kagura sitting",
+    title: "Kagura",
+    dateEnd: "2026/05/23",
+    description: "",
+    link: "https://www.youtube.com/watch?v=KgG9zU5KXoI",
+    image: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/902fc45c-deef-4f0b-b2ac-8e99318e9ac7/dm3qkm7-8952d60c-91af-485f-9b05-7265576603bb.jpg/v1/fill/w_1192,h_670,q_70,strp/kagura_by_lavennielil_dm3qkm7-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6Ii9mLzkwMmZjNDVjLWRlZWYtNGYwYi1iMmFjLThlOTkzMThlOWFjNy9kbTNxa203LTg5NTJkNjBjLTkxYWYtNDg1Zi05YjA1LTcyNjU1NzY2MDNiYi5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.9uvmWY5wrrMJbl8GiDW0wks3NUeh84qBfGTlf5gdIRA",
+    x: 53,
+    yGapAfter: 200
+};
+
+export default meta;
