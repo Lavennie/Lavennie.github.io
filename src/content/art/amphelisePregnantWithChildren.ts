d6dd95250449995ea446cd78581a28b14b563964
Lavennie <42303947+Lavennie@users.noreply.github.com>
2026-07-25 20:23:24 +0200

    Upload art

---
+++ src/content/art/amphelisePregnantWithChildren.ts
@@ -0,0 +1,15 @@
+import type { ArtMeta } from "../types";
+
+const meta: ArtMeta = {
+    type: "art",
+    id: "amphelise pregnant with children",
+    title: "Amphelise with children",
+    dateEnd: "2026/07/13",
+    description: "",
+    link: "https://www.youtube.com/watch?v=204jW4C-Gac",
+    image: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/902fc45c-deef-4f0b-b2ac-8e99318e9ac7/dmfpnq2-cd11dc93-b919-48aa-b434-ba0288794dee.jpg/v1/fill/w_1192,h_670,q_70,strp/amphelise_by_lavennielil_dmfpnq2-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6Ii9mLzkwMmZjNDVjLWRlZWYtNGYwYi1iMmFjLThlOTkzMThlOWFjNy9kbWZwbnEyLWNkMTFkYzkzLWI5MTktNDhhYS1iNDM0LWJhMDI4ODc5NGRlZS5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.oD5gbV3vmR5EdvRmsxxSNs-4Pq990Ctm6J8p7xvZeyU",
+    x: 53,
+    yGapAfter: 200
+};
+
+export default meta;
